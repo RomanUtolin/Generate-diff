740583b6ed97df7e52b0a40aec9ca0682a6ea7ce
Roman Utolin <romautolin@gmail.com>
2022-06-07 22:39:25 +0300

    new optional arguments

--- gendiff/scripts/gendiff.py
+++
@@ -1,8 +0,0 @@
-import argparse
-
-
-parser = argparse.ArgumentParser(prog='gendiff', usage='%(prog)s [options] <filepath1> <filepath1>', description='Compares two configuration files and shows a difference.')
-parser.add_argument('first_file')
-parser.add_argument('second_file')
-args = parser.parse_args()
-print(args.indir)
---
+++ gendiff/scripts/main.py
@@ -0,0 +1,11 @@
+import argparse
+
+
+parser = argparse.ArgumentParser(prog='gendiff', usage='%(prog)s [-h] [-f FORMAT] first_file second_file', description='Compares two configuration files and shows a difference.')
+parser.add_argument('first_file')
+parser.add_argument('second_file')
+parser.add_argument("-f", "--format",
+                    help="set format of output",
+                    )
+args = parser.parse_args()
+print(args.indir)
