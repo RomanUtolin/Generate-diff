847fe0c9cbb3a67ba810fba7cf8a51eecf514077
Roman Utolin <romautolin@gmail.com>
2022-07-08 22:54:16 +0300

    Add gen_dif and open_file Functions

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,5 +1,31 @@
-import open_file
+from gendiff import open_file
 
 
 def generate_diff(path1, path2):
-    pass
+    file1 = open_file.load_file(path1)
+    file2 = open_file.load_file(path2)
+    set_keys = sorted(list(set(list(file1.keys()) + list(file2.keys()))))
+    file1_keys = list(file1.keys())
+    file2_keys = list(file2.keys())
+    result = dict()
+    for i in set_keys:
+        if i in file1_keys and i in file2_keys:
+            if file1[i] == file2[i]:
+                result['  ' + i] = file1[i]
+            else:
+                result['- ' + i] = file1[i]
+                result['+ ' + i] = file2[i]
+        if i in file1_keys and i not in file2_keys:
+            result['- ' + i] = file1[i]
+        if i in file2_keys and i not in file1_keys:
+            result['+ ' + i] = file2[i]
+
+    result_str = ['{']
+    for key, item in result.items():
+        if item is False or True:
+            result_str.append(f'{key}: {str(item).lower()}')
+        else:
+            result_str.append(f'{key}: {item}')
+    result_str.append('}')
+    diff = '\n'.join(result_str)
+    return diff
--- gendiff/open_file.py
+++ gendiff/open_file.py
@@ -1,7 +1,6 @@
 import json
 
 
-def open_file(path1,path2):
-    json1 = json.load(open(str(path1)))
-    json2 = json.load(open(str(path2)))
-    return json1, json2
+def load_file(path):
+    with open(path) as f:
+        return json.load(f)
