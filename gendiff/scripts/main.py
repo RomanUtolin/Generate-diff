0d0f9a29318164f26afb354816796cc405ebde9f
Roman Utolin <romautolin@gmail.com>
2022-08-10 13:08:34 +0300

    fix gen_diff

--- gendiff/scripts/main.py
+++ gendiff/scripts/main.py
@@ -1,5 +1,5 @@
 import argparse
-from gendiff.gen_diff import generate_diff
+from gendiff import gen_diff
 
 parser = argparse.ArgumentParser(prog='gendiff', usage='%(prog)s [-h] [-f FORMAT] first_file second_file',
                                  description='Compares two configuration files and shows a difference.')
@@ -10,4 +10,4 @@ parser.add_argument('-f', '--format',
                     )
 
 args = parser.parse_args()
-print(generate_diff(args.first_file, args.second_file))
+print(gen_diff.generate_diff(args.first_file, args.second_file))
