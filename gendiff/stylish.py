f233da3b4be38af6af9720b490523dd02beda4ac
Roman Utolin <romautolin@gmail.com>
2022-09-07 19:36:58 +0300

    refactoring stylish

--- gendiff/stylish.py
+++ gendiff/stylish.py
@@ -1,40 +1,11 @@
 import itertools
 
-
-def stylish(current_value, depth=0):
-    if not isinstance(current_value, dict):
-        return str(current_value)
-    replacer = ' '
-    spaces_count = 4
-    deep_indent_size = depth + spaces_count
-    current_indent = replacer * depth
-    deep_indent = replacer * deep_indent_size
-    lines = []
-    dict_ = {
-        'nested': [0, ''],
-        'unc': [0, ''],
-        'add': [-2, '+ '],
-        'rm': [-2, '- '],
-        'chang': [-2, '- ', '+ ']
-    }
-    for key, val in current_value.items():
-        if isinstance(val, list):
-            format_bool_val(val)
-            if val[0] == 'chang':
-                lines.append(f'{deep_indent[2:]}- {key}:'
-                             f' {stylish(val[1], deep_indent_size)}')
-                key = f'{deep_indent[2:]}+ {key}'
-                val = val[2]
-            else:
-                key = f'{(dict_[val[0]][0] + deep_indent_size) * replacer}' \
-                      f'{dict_[val[0]][1]}{key}'
-                val = val[1]
-            lines.append(f'{key}: {stylish(val, deep_indent_size)}')
-        else:
-            lines.append(f'{deep_indent}{key}:'
-                         f' {stylish(val, deep_indent_size)}')
-    result = itertools.chain("{", lines, [current_indent + "}"])
-    return '\n'.join(result)
+NEW_REPLACER = {
+    'nested': '  ',
+    'unc': '  ',
+    'add': '+ ',
+    'rm': '- ',
+}
 
 
 def format_bool_val(val):
@@ -44,3 +15,30 @@ def format_bool_val(val):
         elif v is None:
             val[i] = 'null'
     return val
+
+
+def stylish(current_value, depth=0):
+    if not isinstance(current_value, dict):
+        return str(current_value)
+    replacer = ' '
+    spaces_count = 4
+    deep_indent_size = depth + spaces_count
+    deep_indent = replacer * deep_indent_size
+    lines = []
+    for key, val in current_value.items():
+        if isinstance(val, list):
+            format_bool_val(val)
+            if 'chang' in val:
+                add_key = f'{deep_indent[2:]}- {key}'
+                lines.append(f'{add_key}: {stylish(val[1], deep_indent_size)}')
+                add_key = f'{deep_indent[2:]}+ {key}'
+                val = val[2]
+            else:
+                add_key = f'{deep_indent[2:]}{NEW_REPLACER[val[0]]}{key}'
+                val = val[1]
+            lines.append(f'{add_key}: {stylish(val, deep_indent_size)}')
+        else:
+            lines.append(f'{deep_indent}{key}:'
+                         f' {stylish(val, deep_indent_size)}')
+    result = itertools.chain("{", lines, [replacer * depth + "}"])
+    return '\n'.join(result)
